6be2e9359120f8bf7d8f52d4d31b54a6459614c4
androkumar04 <androkumar04@gmail.com>
2015-08-03 22:12:21 +0530

    fixed font size issue

--- static/js/ngletteravatar.js
+++ static/js/ngletteravatar.js
@@ -16,14 +16,12 @@ catalyst.directive('ngLetterAvatar', function() {
 
         var colors = ["#1abc9c", "#16a085", "#f1c40f", "#f39c12", "#2ecc71", "#27ae60", "#e67e22", "#d35400", "#3498db", "#2980b9", "#e74c3c", "#c0392b", "#9b59b6", "#8e44ad", "#bdc3c7", "#34495e", "#2c3e50", "#95a5a6", "#7f8c8d", "#ec87bf", "#d870ad", "#f69785", "#9ba37e", "#b49255", "#b49255", "#a94136"];
 		
-		 var random = ["#8e44ad", "#bdc3c7", "#34495e", "#2c3e50", "#95a5a6", "#7f8c8d", "#ec87bf", "#d870ad", "#f69785", "#9ba37e", "#b49255", "#b49255", "#a94136"];
-
 					var charCount = attrs.charcount;
 					var data = attrs.data;
 					var textColor = '#ffffff';
 					var height = attrs.height;
 					var width = attrs.width;
-					var fontSize = attrs.fontSize;
+					var fontsize = attrs.fontsize;
 					var fontWeight = attrs.fontweight;
 					var fontFamily = attrs.fontfamily;
 					var avatarBorderStyle = attrs.avatarcustomborder;
@@ -37,8 +35,8 @@ catalyst.directive('ngLetterAvatar', function() {
                     if (!textColor) {
                         textColor = '#ffffff';
                     }
-					if (!fontSize) {
-                        fontSize = 30;
+					if (!fontsize) {
+                        fontsize = 30;
                     }
 					if (!height) {
                         height = 50;
@@ -54,17 +52,7 @@ catalyst.directive('ngLetterAvatar', function() {
                     }
 					var c = data.substr(0, charCount).toUpperCase();
 			
-					var cobj = $('<text text-anchor="middle"></text>').attr({
-						'y': '50%',
-						'x': '50%',
-						'dy' : '0.35em',
-						'pointer-events':'auto',
-						'fill': textColor,
-						'font-family': fontFamily
-					}).html(c).css({
-						'font-weight': fontWeight,
-						'font-size': fontSize+'px',
-					});
+					var cobj = getCharacterObject(c,textColor,fontFamily,fontWeight,fontsize);
 				var colorIndex = '';
 				var color = '';
 				if(c.charCodeAt(0) < 65){
@@ -80,38 +68,24 @@ catalyst.directive('ngLetterAvatar', function() {
 				}
 				
 			
-				var svg = $('<svg></svg>').attr({
-					'xmlns': 'http://www.w3.org/2000/svg',
-					'pointer-events':'none',
-					'width': width,
-					'height': height
-				}).css({
-					'background-color': color,
-					'width': width+'px',
-					'height': height+'px'
-					//'border': '5px solid red'
-				});
+				    var svg = getImgTag(width,height,color);
 					svg.append(cobj);
 					var svgHtml = window.btoa(unescape(encodeURIComponent($('<div>').append(svg.clone()).html())));	
 					var component;
 					var base = 'data:image/svg+xml;base64,';
 					var _style = '';
 					    if(avatarBorderStyle){
-						 console.log('if avatarBorderStyle');
 							    _style = avatarBorderStyle;
 						}else if(avatardefaultBorder){
-						console.log('if avatardefaultBorder');
 								_style = defaultBorder;
 						}					
 					
 					if(shape){
 						if(shape ==='round'){
 							var round_style = "border-radius:30px;"+_style;
-							console.log("round style"+round_style)
 							component = "<img src="+base+svgHtml+" style='"+round_style+"' />";
 						}
 					}else{
-						console.log("square style"+_style)
 						component = "<img src="+base+svgHtml+" style='"+_style+"' />";
 					}
 					element.append(component);
@@ -119,4 +93,33 @@ catalyst.directive('ngLetterAvatar', function() {
             };
         });
 
+		function getImgTag(width,height,color){
+			var svg = $('<svg></svg>').attr({
+						'xmlns': 'http://www.w3.org/2000/svg',
+						'pointer-events':'none',
+						'width': width,
+						'height': height
+					}).css({
+						'background-color': color,
+						'width': width+'px',
+						'height': height+'px'
+						//'border': '5px solid red'
+					});
+			return svg;
+		}
+		
+		function getCharacterObject(character,textColor,fontFamily,fontWeight,fontsize){
+				var cobj = $('<text text-anchor="middle"></text>').attr({
+						'y': '50%',
+						'x': '50%',
+						'dy' : '0.35em',
+						'pointer-events':'auto',
+						'fill': textColor,
+						'font-family': fontFamily
+					}).html(character).css({
+						'font-weight': fontWeight,
+						'font-size': fontsize+'px',
+					});
+			return cobj;
+		}
     
