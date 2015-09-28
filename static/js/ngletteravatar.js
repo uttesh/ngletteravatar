959e9659ad8f9387a7c6138577ca37cacb99db5e
androkumar04 <androkumar04@gmail.com>
2015-09-28 23:34:21 +0530

    updated the plugin to 3.0.0 version

--- static/js/ngletteravatar.js
+++ static/js/ngletteravatar.js
@@ -1,88 +1,78 @@
 /**
  * NG Letter Avatar is directive for AngularJS apps
- * @version v2.0.1 - 2015-05-25 * @link https://github.com/uttesh/ngletteravatar
+ * @version v3.0.0 - 2015-09-28 * @link https://github.com/uttesh/ngletteravatar
  * @author Uttesh Kumar T.H. <uttesh@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
 'use strict';
 
-var catalyst = angular.module('ngLetterAvatar', []);
+var nla = angular.module('ngLetterAvatar', []);
 
+nla.constant('defaultSettings', {
+    alphabetcolors: ["#5A8770", "#B2B7BB", "#6FA9AB", "#F5AF29", "#0088B9", "#F18636", "#D93A37", "#A6B12E", "#5C9BBC", "#F5888D", "#9A89B5", "#407887", "#9A89B5", "#5A8770", "#D33F33", "#A2B01F", "#F0B126", "#0087BF", "#F18636", "#0087BF", "#B2B7BB", "#72ACAE", "#9C8AB4", "#5A8770", "#EEB424", "#407887"],
+    textColor: '#ffffff',
+    defaultBorder: 'border:5px solid white',
+	fontsize: 30, // unit in pixels
+	height: 50, // unit in pixels
+	width: 50, // unit in pixels
+	fontWeight:400, //
+	charCount:1,
+	fontFamily:'HelveticaNeue-Light,Helvetica Neue Light,Helvetica Neue,Helvetica, Arial,Lucida Grande, sans-serif',
+	base:'data:image/svg+xml;base64,',
+	radius:'border-radius:30px;'
 
-catalyst.directive('ngLetterAvatar', function() {
+});
+
+nla.directive('ngLetterAvatar', ['defaultSettings',function(defaultSettings) {
             return {
                 restrict: 'E',
+				//replace: true,
                 link: function(scope, element, attrs) {
-
-        var colors = ["#1abc9c", "#16a085", "#f1c40f", "#f39c12", "#2ecc71", "#27ae60", "#e67e22", "#d35400", "#3498db", "#2980b9", "#e74c3c", "#c0392b", "#9b59b6", "#8e44ad", "#bdc3c7", "#34495e", "#2c3e50", "#95a5a6", "#7f8c8d", "#ec87bf", "#d870ad", "#f69785", "#9ba37e", "#b49255", "#b49255", "#a94136"];
-		
-					var charCount = attrs.charcount;
-					var data = attrs.data;
-					var textColor = '#ffffff';
-					var height = attrs.height;
-					var width = attrs.width;
-					var fontsize = attrs.fontsize;
-					var fontWeight = attrs.fontweight;
-					var fontFamily = attrs.fontfamily;
-					var avatarBorderStyle = attrs.avatarcustomborder;
-					var avatardefaultBorder = attrs.avatarborder;
-					var defaultBorder = "border:5px solid white";
 					
-					var shape = attrs.shape;
-                    if (!charCount) {
-                        charCount = 1;
-                    }
-                    if (!textColor) {
-                        textColor = '#ffffff';
-                    }
-					if (!fontsize) {
-                        fontsize = 30;
-                    }
-					if (!height) {
-                        height = 50;
-                    }
-					if (!width) {
-                        width = 50;
-                    }
-					if (!fontWeight) {
-                        fontWeight = 400;
-                    }
-					if (!fontFamily) {
-                        fontFamily = 'HelveticaNeue-Light,Helvetica Neue Light,Helvetica Neue,Helvetica, Arial,Lucida Grande, sans-serif';
-                    }
-					var c = data.substr(0, charCount).toUpperCase();
-			
-					var cobj = getCharacterObject(c,textColor,fontFamily,fontWeight,fontsize);
-				var colorIndex = '';
-				var color = '';
-				if(c.charCodeAt(0) < 65){
-					var letters = '0123456789ABCDEF'.split('');
-					var _color = '#';
-						for (var i = 0; i < 6; i++ ) {
-							_color += letters[Math.floor(Math.random() * 16)];
-						}
-					color = _color;
-				}else{
-				    colorIndex = Math.floor((c.charCodeAt(0) - 65) % colors.length);
-					color =  colors[colorIndex];
-				}
+					var params = {
+						charCount:isNotNull(attrs.charcount) ? attrs.charcount : defaultSettings.charCount,
+						data:attrs.data,
+						textColor:defaultSettings.textColor,
+						height:isNotNull(attrs.height) ? attrs.height : defaultSettings.height,
+						width:isNotNull(attrs.width) ? attrs.width : defaultSettings.width,
+						fontsize:isNotNull(attrs.fontsize) ? attrs.fontsize : defaultSettings.fontsize,
+						fontWeight:isNotNull(attrs.fontweight) ? attrs.fontweight : defaultSettings.fontWeight,
+						fontFamily:isNotNull(attrs.fontfamily) ? attrs.fontfamily : defaultSettings.fontFamily,
+						avatarBorderStyle:attrs.avatarcustomborder,
+						avatardefaultBorder:attrs.avatarborder,
+						defaultBorder:defaultSettings.defaultBorder,
+						shape:attrs.shape
+					};
+
+					var c = params.data.substr(0, params.charCount).toUpperCase();
+					var cobj = getCharacterObject(c,params.textColor,params.fontFamily,params.fontWeight,params.fontsize);
+					var colorIndex = '';
+					var color = '';
+					
+					if(c.charCodeAt(0) < 65){
+						color = getRandomColors();
+					}else{
+						colorIndex = Math.floor((c.charCodeAt(0) - 65) % defaultSettings.alphabetcolors.length);
+						color =  defaultSettings.alphabetcolors[colorIndex];
+					}
 				
 			
-				    var svg = getImgTag(width,height,color);
+				    var svg = getImgTag(params.width,params.height,color);
 					svg.append(cobj);
-					var svgHtml = window.btoa(unescape(encodeURIComponent($('<div>').append(svg.clone()).html())));	
+					var lvcomponent = angular.element('<div>').append(svg.clone()).html();
+					var svgHtml = window.btoa(unescape(encodeURIComponent(lvcomponent)));	
 					var component;
-					var base = 'data:image/svg+xml;base64,';
+					var base = defaultSettings.base;
 					var _style = '';
-					    if(avatarBorderStyle){
-							    _style = avatarBorderStyle;
-						}else if(avatardefaultBorder){
-								_style = defaultBorder;
+					    if(params.avatarBorderStyle){
+							    _style = params.avatarBorderStyle;
+						}else if(params.avatardefaultBorder){
+								_style = params.defaultBorder;
 						}					
 					
-					if(shape){
-						if(shape ==='round'){
-							var round_style = "border-radius:30px;"+_style;
+					if(params.shape){
+						if(params.shape ==='round'){
+							var round_style = defaultSettings.radius+_style;
 							component = "<img src="+base+svgHtml+" style='"+round_style+"' />";
 						}
 					}else{
@@ -91,35 +81,58 @@ catalyst.directive('ngLetterAvatar', function() {
 					element.append(component);
                 }
             };
-        });
+        }]);
+
+
+        function getRandomColors(){
+			var letters = '0123456789ABCDEF'.split('');
+						var _color = '#';
+							for (var i = 0; i < 6; i++ ) {
+								_color += letters[Math.floor(Math.random() * 16)];
+							}
+			return _color;
+		}			
+		function isNotNull(obj){
+			if(obj){
+				return true;
+			}
+			return false;
+		}
 
 		function getImgTag(width,height,color){
-			var svg = $('<svg></svg>').attr({
-						'xmlns': 'http://www.w3.org/2000/svg',
-						'pointer-events':'none',
-						'width': width,
-						'height': height
-					}).css({
-						'background-color': color,
-						'width': width+'px',
-						'height': height+'px'
-						//'border': '5px solid red'
-					});
-			return svg;
+			
+			var svgTag = angular.element('<svg></svg>')
+			                    .attr({
+										'xmlns': 'http://www.w3.org/2000/svg',
+										'pointer-events':'none',
+										'width': width,
+										'height': height
+									})
+								.css({
+										'background-color': color,
+										'width': width+'px',
+										'height': height+'px'
+									});
+				
+			return svgTag;
 		}
 		
 		function getCharacterObject(character,textColor,fontFamily,fontWeight,fontsize){
-				var cobj = $('<text text-anchor="middle"></text>').attr({
-						'y': '50%',
-						'x': '50%',
-						'dy' : '0.35em',
-						'pointer-events':'auto',
-						'fill': textColor,
-						'font-family': fontFamily
-					}).html(character).css({
-						'font-weight': fontWeight,
-						'font-size': fontsize+'px',
-					});
-			return cobj;
+				 var textTag =	angular.element('<text text-anchor="middle"></text>')
+					       .attr({
+									'y': '50%',
+									'x': '50%',
+									'dy' : '0.35em',
+									'pointer-events':'auto',
+									'fill': textColor,
+									'font-family': fontFamily
+								})
+						   .html(character)
+						   .css({
+									'font-weight': fontWeight,
+									'font-size': fontsize+'px',
+								});
+				
+			return textTag;
 		}
     
