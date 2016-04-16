ccde0bb3729753907436229e18b0898dd34d2501
androkumar04 <androkumar04@gmail.com>
2016-04-16 12:39:52 +0530

    Updated with 4.0.2 version

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ var myapp = angular.module('letterAvatarApp', ['ngLetterAvatar']);
 
 myapp.controller("letterAvatarCtrl", ['$scope', function($scope) {
 
-
+     $scope.dynamicData = "Testing";
      $scope.customColors = ["#D93A37", "#5A8770", "#5A8770", "#5A8770", "#5A8770", "#5A8770", "#5A8770", "#5A8770", "#5C9BBC", "#F5888D", "#9A89B5", "#407887", "#9A89B5", "#5A8770", "#D33F33", "#A2B01F", "#F0B126", "#0087BF", "#F18636", "#0087BF", "#B2B7BB", "#72ACAE", "#9C8AB4", "#5A8770", "#EEB424", "#407887"];
 }]);
 
